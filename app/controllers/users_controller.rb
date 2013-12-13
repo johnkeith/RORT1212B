1796855620f5bb1b404b7b0aa109cc05637507d4
John Keith <johnkeith@outlook.com>
2013-12-13 15:29:13 -0500

    to 9.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,12 @@ class UsersController < ApplicationController
   			render 'new'
   		end
   	end
-
-  	  def user_params
-  	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  	  end
+ end
+  	
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+ 
 
   def show
     @user = User.find(params[:id])
