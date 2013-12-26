e40d64cc6f34331a320f0e495ad7e1eb500417ba
John Keith <johnkeith@outlook.com>
2013-12-26 08:45:01 -0500

    up to 9.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
   def new
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
   		end
   	end
  end 
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
