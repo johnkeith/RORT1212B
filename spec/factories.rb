0c0f6e8662ba8a309cbb02a7e0f0815e0fae2fe9
John Keith <johnkeith@outlook.com>
2014-01-11 11:07:12 -0500

    finish chapter 9 but admin not working

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "Johnny"
-    email "johnny@gmail.com"
-    password "fizzbuzz"
-    password_confirmation "fizzbuzz"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
